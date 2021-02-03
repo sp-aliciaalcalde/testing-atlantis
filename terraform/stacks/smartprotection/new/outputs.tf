291c1a84b0ae741efcac045a3fe957d24919ee66
Alicia Alcalde Delgado <alicia.alcalde@smartprotection.com>
2021-02-03 11:37:44 +0100

    Update stack smartprotection

--- terraform/stacks/smartprotection/new/outputs.tf
+++ terraform/stacks/smartprotection/new/outputs.tf
@@ -15,6 +15,6 @@ output "vpc_flow_log_id" { value = module.vpc.vpc_flow_log_id }
 output "vpc_id" { value = module.vpc.vpc_id }
 
 # ECS Cluster
-output "ecs_cluster_arn" { value = module.ecs_cluster.this_ecs_cluster_arn }
-output "ecs_cluster_id" { value = module.ecs_cluster.this_ecs_cluster_id }
-output "ecs_cluster_name" { value = module.ecs_cluster.this_ecs_cluster_name }
+#output "ecs_cluster_arn" { value = module.ecs_cluster.this_ecs_cluster_arn }
+#output "ecs_cluster_id" { value = module.ecs_cluster.this_ecs_cluster_id }
+#output "ecs_cluster_name" { value = module.ecs_cluster.this_ecs_cluster_name }
