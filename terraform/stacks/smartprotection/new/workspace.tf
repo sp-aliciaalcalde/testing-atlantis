a1c9b73bd698b7e8eacf435f68e2f8eff654c01a
Alicia Alcalde Delgado <alicia.alcalde@smartprotection.com>
2021-02-03 15:17:05 +0100

    ARN_Assume_Role

--- terraform/stacks/smartprotection/new/workspace.tf
+++ terraform/stacks/smartprotection/new/workspace.tf
@@ -12,7 +12,7 @@ locals {
   workspaces     = merge(local.context_variables)
   workspace      = merge(local.workspaces[terraform.workspace])
   tags           = merge(var.tags, map("project", var.project), map("environment", terraform.workspace))
-  name_prefix    = "testing-atlantis-${terraform.workspace}-${var.project}"
+  name_prefix    = "testing-atlantis-1-${terraform.workspace}-${var.project}"
   azs            = [data.aws_availability_zones.available.names[0], data.aws_availability_zones.available.names[1]]
   public_cidrs   = [cidrsubnet(local.workspace["vpc_cidr"], 8, 1), cidrsubnet(local.workspace["vpc_cidr"], 8, 2)]
   private_cidrs  = concat([cidrsubnet(local.workspace["vpc_cidr"], 8, 11), cidrsubnet(local.workspace["vpc_cidr"], 8, 12)], local.lambda_cidrs)
